dddc51e835b0c6191db7c78a00132089439f1fef
Nozomi Isozaki <niszkml@gmail.com>
2020-07-18 11:09:17 +0900

    Add build_linux.sh

---
+++ scripts/build_linux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+readonly proj_dir="$(dirname "$0")/.."
+readonly build_dir="$proj_dir/build-linux"
+readonly cc="gcc"
+
+rm -fr "$build_dir"
+cp -r "$proj_dir/quickjs-src" "$build_dir"
+make -C "$build_dir" QJSC_CC="$cc" CC="$cc"
