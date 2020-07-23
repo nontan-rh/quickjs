fa02fdda5c03dc73662819fc1ebae0197ce9c656
Nozomi Isozaki <niszkml@gmail.com>
2020-07-23 14:05:58 +0900

    Add original makefile

--- Makefile
+++ Makefile
@@ -1,33 +1,29 @@
-EMSCRIPTEN_BUILD_DIR=build_emscripten
-EMSCRIPTEN_BUILD_OPTIONS=CONFIG_CLANG=y CROSS_PREFIX=placeholder-cross-prefix- HOST_CC=clang CC=emcc QJSC_CC=emcc AR=emar STRIP=true EXE=.js
+EMSCRIPTEN_BUILD_OPTIONS=HOST_CC=gcc TARGET_CC=emcc AR=emar TARGET_EXE=.js
+LINUX_BUILD_OPTIONS=HOST_CC=gcc TARGET_CC=gcc
 
-LINUX_BUILD_DIR=build_linux
-LINUX_BUILD_OPTIONS=
+SIMPLIFIED_MAKEFILE=quickjs.mk
 
 export
 
 all:
 
-$(EMSCRIPTEN_BUILD_DIR): quickjs-src
-	rm -fr "$(EMSCRIPTEN_BUILD_DIR)"
-	cp -r quickjs-src "$(EMSCRIPTEN_BUILD_DIR)"
-
-emscripten: $(EMSCRIPTEN_BUILD_DIR)
-	$(MAKE) -C $(EMSCRIPTEN_BUILD_DIR) $(EMSCRIPTEN_BUILD_OPTIONS)
-
-emscripten-install: emscripten
-	$(MAKE) -C $(EMSCRIPTEN_BUILD_DIR) $(EMSCRIPTEN_BUILD_OPTIONS) install
+install-cmake:
 	mkdir -p "$(DESTDIR)/lib/cmake/quickjs"
 	install -m644 cmake/quickjs-config.cmake "$(DESTDIR)/lib/cmake/quickjs/quickjs-config.cmake"
 
-$(LINUX_BUILD_DIR): quickjs-src
-	rm -fr "$(LINUX_BUILD_DIR)"
-	cp -r quickjs-src "$(LINUX_BUILD_DIR)"
+emscripten:
+	$(MAKE) -f $(SIMPLIFIED_MAKEFILE) $(EMSCRIPTEN_BUILD_OPTIONS)
 
-linux: $(LINUX_BUILD_DIR)
-	$(MAKE) -C $(LINUX_BUILD_DIR) $(LINUX_BUILD_OPTIONS)
+emscripten-install: emscripten install-cmake
+	$(MAKE) -f $(SIMPLIFIED_MAKEFILE) $(EMSCRIPTEN_BUILD_OPTIONS) install
 
-linux-install: linux
-	$(MAKE) -C $(LINUX_BUILD_DIR) $(LINUX_BUILD_OPTIONS) install
-	mkdir -p "$(DESTDIR)/lib/quickjs/cmake"
-	install -m644 cmake/quickjs-config.cmake "$(DESTDIR)/lib/quickjs/cmake/quickjs-config.cmake"
+linux:
+	$(MAKE) -f $(SIMPLIFIED_MAKEFILE) $(LINUX_BUILD_OPTIONS)
+
+linux-install: linux install-cmake
+	$(MAKE) -f $(SIMPLIFIED_MAKEFILE) $(LINUX_BUILD_OPTIONS) install
+
+clean:
+	$(MAKE) -f $(SIMPLIFIED_MAKEFILE) $(EMSCRIPTEN_BUILD_OPTIONS) clean
+	$(MAKE) -f $(SIMPLIFIED_MAKEFILE) $(LINUX_BUILD_OPTIONS) clean
+	rm -f qjs.wasm
---
+++ quickjs.mk
@@ -0,0 +1,109 @@
+
+OBJDIR=.obj
+
+TARGET_CC=clang
+HOST_CC=clang
+
+TARGET_AR=ar
+HOST_AR=ar
+
+TARGET_EXE=
+HOST_EXE=
+
+TARGET_LIBS=-lm -ldl -lpthread
+HOST_LIBS=-lm -ldl -lpthread
+
+ADDITIONAL_HOST_CFLAGS=
+ADDITIONAL_HOST_LDFLAGS=
+ADDITIONAL_TARGET_CFLAGS=
+ADDITIONAL_TARGET_LDFLAGS=
+
+CFLAGS_CLANG=-Wall -MMD -MF $(OBJDIR)/$(@F).d
+CFLAGS_CLANG+= -Wextra
+CFLAGS_CLANG+= -Wno-sign-compare
+CFLAGS_CLANG+= -Wno-missing-field-initializers
+CFLAGS_CLANG+= -Wundef -Wuninitialized
+CFLAGS_CLANG+= -Wunused -Wno-unused-parameter
+CFLAGS_CLANG+= -Wwrite-strings
+CFLAGS_CLANG+= -Wchar-subscripts -funsigned-char
+CFLAGS_CLANG+= -MMD -MF $(OBJDIR)/$(@F).d
+
+LDFLAGS_CLANG=
+
+CFLAGS_GCC=-Wall -MMD -MF $(OBJDIR)/$(@F).d
+CFLAGS_GCC+= -Wno-array-bounds -Wno-format-truncation
+
+LDFLAGS_GCC=
+
+DEFINES:=-D_GNU_SOURCE -DCONFIG_VERSION=\"$(shell cat ./quickjs-src/VERSION)\"
+
+ifneq ($(findstring clang,$(TARGET_CC)),)
+  TARGET_CFLAGS=$(CFLAGS_CLANG) $(DEFINES)
+  TARGET_LDFLAGS=$(LDFLAGS_CLANG)
+else ifneq ($(findstring emcc,$(TARGET_CC)),)
+  TARGET_CFLAGS=$(CFLAGS_CLANG) $(DEFINES)
+  TARGET_LDFLAGS=$(LDFLAGS_CLANG)
+else
+  TARGET_CFLAGS=$(CFLAGS_GCC) $(DEFINES)
+  TARGET_LDFLAGS=$(LDFLAGS_GCC)
+endif
+
+TARGET_CFLAGS+= $(ADDITIONAL_TARGET_CFLAGS)
+TARGET_LDFLAGS+= $(ADDITIONAL_TARGET_LDFLAGS)
+
+ifneq ($(findstring clang,$(HOST_CC)),)
+  HOST_CFLAGS=$(CFLAGS_CLANG) $(DEFINES)
+  HOST_LDFLAGS=$(LDFLAGS_CLANG)
+else ifneq ($(findstring emcc,$(HOST_CC)),)
+  HOST_CFLAGS=$(CFLAGS_CLANG) $(DEFINES)
+  HOST_LDFLAGS=$(LDFLAGS_CLANG)
+else
+  HOST_CFLAGS=$(CFLAGS_GCC) $(DEFINES)
+  HOST_LDFLAGS=$(LDFLAGS_GCC)
+endif
+
+HOST_CFLAGS+= $(ADDITIONAL_HOST_CFLAGS)
+HOST_LDFLAGS+= $(ADDITIONAL_HOST_LDFLAGS)
+
+TARGET_QJS_LIB_OBJS=$(OBJDIR)/quickjs.o $(OBJDIR)/libregexp.o $(OBJDIR)/libunicode.o $(OBJDIR)/cutils.o $(OBJDIR)/quickjs-libc.o
+HOST_QJS_LIB_OBJS=$(patsubst %.o, %.host.o, $(TARGET_QJS_LIB_OBJS))
+
+QJSC=qjsc$(HOST_EXE)
+QJS=qjs$(TARGET_EXE)
+PROGS=$(QJSC) $(QJS)
+
+all: libquickjs.a $(PROGS)
+
+$(OBJDIR):
+	mkdir -p $(OBJDIR)
+
+$(OBJDIR)/%.o: quickjs-src/%.c | $(OBJDIR)
+	$(TARGET_CC) $(TARGET_CFLAGS) -c -o $@ $<
+
+$(OBJDIR)/%.host.o: quickjs-src/%.c | $(OBJDIR)
+	$(HOST_CC) $(HOST_CFLAGS) -c -o $@ $<
+
+libquickjs.a: $(TARGET_QJS_LIB_OBJS)
+	$(TARGET_AR) rcs $@ $^
+
+$(QJSC): $(OBJDIR)/qjsc.host.o $(HOST_QJS_LIB_OBJS)
+	$(HOST_CC) $(HOST_LDFLAGS) -o $@ $^ $(HOST_LIBS)
+
+$(OBJDIR)/repl.o: $(QJSC) quickjs-src/repl.js
+	./$(QJSC) -c -o $(OBJDIR)/repl.c -m quickjs-src/repl.js
+	$(TARGET_CC) $(TARGET_CFLAGS) -c -o $(OBJDIR)/repl.o $(OBJDIR)/repl.c
+
+$(QJS): $(OBJDIR)/repl.o $(OBJDIR)/qjs.o $(TARGET_QJS_LIB_OBJS)
+	$(TARGET_CC) $(TARGET_LDFLAGS) -o $@ $^ $(TARGET_LIBS)
+
+clean:
+	rm -f *.a $(PROGS)
+	rm -rf $(OBJDIR)/
+
+install: all
+	mkdir -p "$(DESTDIR)/bin"
+	install -m755 qjsc$(HOST_EXE) "$(DESTDIR)/bin"
+	mkdir -p "$(DESTDIR)/lib/quickjs"
+	install -m644 libquickjs.a "$(DESTDIR)/lib/quickjs"
+	mkdir -p "$(DESTDIR)/include/quickjs"
+	install -m644 quickjs.h quickjs-libc.h "$(DESTDIR)/include/quickjs"
